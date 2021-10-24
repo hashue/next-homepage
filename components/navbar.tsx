52a61eca9e4ba847ab4c933a4d8dd8c5f5054b7b
Hasu <hshue83@gmail.com>
2021-10-24 19:17:09 +0900

    intro tailwindcss and apply global css

---
+++ components/navbar.tsx
@@ -0,0 +1,15 @@
+import { Menu, MenuList, MenuItem } from "@chakra-ui/react";
+
+const Nav = () => {
+    return (
+        <div>
+            <Menu>
+                <MenuList>
+                    <MenuItem>Works</MenuItem>
+                </MenuList>
+            </Menu>
+        </div>
+    );
+};
+
+export default Nav;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import Nav from "../components/navbar";
+const Home: NextPage = () => {
+    return (
+        <div>
+            <Nav />
+            <h1>Hasu</h1>
+        </div>
+    );
+};
+
+export default Home;
