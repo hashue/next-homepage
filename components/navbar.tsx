ae561b15dfd0e8e624089587c5f73a1a0234155a
Hasu <hshue83@gmail.com>
2021-10-27 08:28:14 +0900

    [improve] organize packages and format file

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 import styled from "@emotion/styled";
 
 const NavLinkItem = styled.div`
-    margin: ${props => props.$title ? ".4rem  0 2rem" : " 1.5rem 0 0 0 "};
+    margin: ${(props) => (props.$title ? ".4rem  0 2rem" : " 1.5rem 0 0 0 ")};
     flex: 1 0 auto;
-    font-size: ${props => props.$title ? "2.3rem" : "1.4rem"};
-    font-weight: ${props => props.$title ? "bold" : "600"};
+    font-size: ${(props) => (props.$title ? "2.3rem" : "1.4rem")};
+    font-weight: ${(props) => (props.$title ? "bold" : "600")};
 `;
 
 const Nav = () => {
     return (
-        <div style={{display:"flex", justifyContent:"flex-start", width:"100%"}}>
+        <div style={{ display: "flex", justifyContent: "flex-start", width: "100%" }}>
             <NavLinkItem $title>
                 <Link href="/">Hasu</Link>
             </NavLinkItem>
