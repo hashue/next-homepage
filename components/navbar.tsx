9271d64d66dcc053d53f67372d7f92ce1b68d9cd
Hasu <hshue83@gmail.com>
2021-10-26 11:50:52 +0900

    [feat] impl nav bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,23 +1,22 @@
 import Link from "next/link";
+import styled from "styled-components";
 
-const NavLinkItem = ({ href, link_word, ...props }: any) => {
-    return (
-        <Link href={href} {...props}>
-            {link_word}
-        </Link>
-    );
-};
+const NavLinkItem = styled.div`
+    margin: ${props => props.$title ? ".4rem 0 0 -2.2rem" : " 1.5rem 0 0 0 "};
+    flex: 1 0 auto;
+    font-size: ${props => props.$title ? "2.3rem" : "1.24rem"};
+    font-weight: ${props => props.$title ? "bold" : "600"};
+`;
 
 const Nav = () => {
     return (
         <div className="flex justify-start w-full">
-            <NavLinkItem
-                href="/"
-                link_word="Hasu"
-                style={{ paddingRight: "15rem" }}
-                className="md:pr-60"
-            />
-            <NavLinkItem href="/works" link_word="Works" />
+            <NavLinkItem $title>
+                <Link href="/">Hasu</Link>
+            </NavLinkItem>
+            <NavLinkItem>
+                <Link href="/works">Works</Link>
+            </NavLinkItem>
         </div>
     );
 };
