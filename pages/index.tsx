5b8807bb623c78fb64753a2a22e9a016a81234b0
Hasu <hshue83@gmail.com>
2022-03-04 16:00:30 +0900

    feat: update information

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,56 +5,53 @@ import Footer from "../components/footer";
 import { MyHistory, BioItem, Section } from "../components/history";
 
 const Home: NextPage = () => {
-    return (
-        <div style={{ maxWidth: "700px", width: "50%", margin: "0 auto" }}>
-            <Nav />
-            <CircleLogo src="/images/logo.png" />
+  return (
+    <div style={{ maxWidth: "700px", width: "50%", margin: "0 auto" }}>
+      <Nav />
+      <CircleLogo src="/images/logo.png" />
 
-            <h1 style={{ fontSize: "2.6rem", textAlign: "center", margin: "6rem 0 1rem 0" }}>
-                Hasu
-            </h1>
-            <p style={{ fontWeight: 400, fontSize: "1.25rem", textAlign: "center" }}>
-                -Web developer-
-            </p>
+      <h1 style={{ fontSize: "2.6rem", textAlign: "center", margin: "6rem 0 1rem 0" }}>Hasu</h1>
+      <p style={{ fontWeight: 400, fontSize: "1.25rem", textAlign: "center" }}>-Web developer-</p>
 
-            <MyHistory
-                section="About"
-                description="
+      <MyHistory
+        section="About"
+        description="
               2003年生れ。小学一年生のときにPCを初めて触り興味を持つ。 
             その後小学五年生のときにプログラミングに出会い学習を開始。
             現在はWeb制作を中心に学習中"
-            />
+      />
 
-            <MyHistory section="Biography" description="" />
+      <MyHistory section="Biography" description="" />
 
-            <BioItem year="2010" description="初めてPC(Windows2000)に触れる" />
-            <BioItem year="2015" description="プログラミングに出会う" />
-            <BioItem year="2019" description="学校法人角川ドワンゴ学園N高等学校に転入学" />
-            <BioItem year="2019" description="ITパスポート試験 合格" />
-            <BioItem
-                year="2020"
-                description="おおいたヒューマンフェスティバル webサイト開発メンバーとして参加"
-            />
-            <BioItem year="2020" description="サンシステム アルバイト入社" />
+      <BioItem year="2010" description="初めてPC(Windows2000)に触れる" />
+      <BioItem year="2015" description="プログラミングに出会う" />
+      <BioItem year="2019" description="学校法人角川ドワンゴ学園N高等学校に転入学" />
+      <BioItem year="2019" description="ITパスポート試験 合格" />
+      <BioItem
+        year="2020"
+        description="おおいたヒューマンフェスティバル webサイト開発メンバーとして参加"
+      />
+      <BioItem year="2020" description="サンシステム アルバイト入社" />
+      <BioItem year="2022" description="フルーリオ株式会社 入社" />
 
-            <Section>Platform</Section>
-            <ul>
-                <li style={{ listStyle: "none", marginBottom: "1.3rem" }}>
-                    <a href="https://github.com/hashue/">GitHub</a>
-                </li>
-                <li style={{ listStyle: "none", marginBottom: "1.3rem" }}>
-                    <a href="https://twitter.com/83_hasu/">Twitter</a>
-                </li>
-                <li style={{ listStyle: "none", marginBottom: "1.3rem" }}>
-                    <a href="https://zenn.dev/hasu_83">Zenn</a>
-                </li>
-                <li style={{ listStyle: "none", marginBottom: "1.3rem" }}>
-                    <a href="https://qiita.com/7321hasu">Qiita</a>
-                </li>
-            </ul>
-            <Footer />
-        </div>
-    );
+      <Section>Platform</Section>
+      <ul>
+        <li style={{ listStyle: "none", marginBottom: "1.3rem" }}>
+          <a href="https://github.com/hashue/">GitHub</a>
+        </li>
+        <li style={{ listStyle: "none", marginBottom: "1.3rem" }}>
+          <a href="https://twitter.com/83_hasu/">Twitter</a>
+        </li>
+        <li style={{ listStyle: "none", marginBottom: "1.3rem" }}>
+          <a href="https://zenn.dev/hasu_83">Zenn</a>
+        </li>
+        <li style={{ listStyle: "none", marginBottom: "1.3rem" }}>
+          <a href="https://qiita.com/7321hasu">Qiita</a>
+        </li>
+      </ul>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
--- pages/works.tsx
+++ pages/works.tsx
@@ -6,47 +6,53 @@ import Nav from "../components/navbar";
 import WorkItem from "../components/work";
 
 const Works: NextPage = () => {
-    return (
-        <div style={{ maxWidth: "700px", width: "50%", margin: "0 auto" }}>
-            <Nav />
-            <CircleLogo src="/images/logo.png" />
-            <Section>Works</Section>
-            <div
-                style={{
-                    display: "grid",
-                    gridTemplateColumns: "repeat(auto-fit,minmax(230px,1fr))",
-                    gap: "24px",
-                }}
-            >
-                <WorkItem
-                    img="/images/timer.png"
-                    title="Focusaid"
-                    description="ポモドーロテクニック支援のためのタイマーアプリ
+  return (
+    <div style={{ maxWidth: "700px", width: "50%", margin: "0 auto" }}>
+      <Nav />
+      <CircleLogo src="/images/logo.png" />
+      <Section>Works</Section>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit,minmax(230px,1fr))",
+          gap: "24px",
+        }}
+      >
+        <WorkItem
+          img="/images/timer.png"
+          title="Focusaid"
+          description="ポモドーロテクニック支援のためのタイマーアプリ
                                  Localstrageによるプリセット保存機能あり"
-                    url="https://hashue.github.io/Timer/"
-                />
-                <WorkItem
-                    img="/images/tuberlooper.png"
-                    title="TuberLooper"
-                    description="Youtube IFrame Player APIを用いたループ再生を実現するサイト"
-                    url="https://hashue.github.io/TuberLooper/"
-                />
-                <WorkItem
-                    img="/images/rusmo.png"
-                    title="rusmo"
-                    description="Rust製のメモ作成支援ツール"
-                    url="https://crates.io/crates/rusmo"
-                />
-                <WorkItem
-                    img="/images/old-portfolio.png"
-                    title="ポートフォリオ"
-                    description="旧ポートフォリオ"
-                    url="https://hasu-83.herokuapp.com"
-                />
-            </div>
-            <Footer />
-        </div>
-    );
+          url="https://hashue.github.io/Timer/"
+        />
+        <WorkItem
+          img="/images/tuberlooper.png"
+          title="TuberLooper"
+          description="Youtube IFrame Player APIを用いたループ再生を実現するサイト"
+          url="https://hashue.github.io/TuberLooper/"
+        />
+        <WorkItem
+          img="/images/rusmo.png"
+          title="rusmo"
+          description="Rust製のメモ作成支援ツール"
+          url="https://crates.io/crates/rusmo"
+        />
+        <WorkItem
+          img="/images/old-portfolio.png"
+          title="ポートフォリオ"
+          description="旧ポートフォリオ"
+          url="https://hasu-83.herokuapp.com"
+        />
+        <WorkItem
+          img="/images/frourio-hp.png"
+          title="フルーリオ株式会社HP"
+          description="フルーリオ株式会社のコーポレートサイト デザインから開発まで全てを担当"
+          url="https://frourio.co.jp"
+        />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Works;
